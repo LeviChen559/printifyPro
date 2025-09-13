156ab87c0424fed9563c1e896bf06cfe3a2b68de
levi <levi@studypug.com>
2025-09-13 13:03:04 -0700

    Refactor LabelLogo component to adjust logo dimensions based on isTable prop for improved responsiveness

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -10,19 +10,20 @@ import { Container } from "./style";
 import React from "react";
 interface iProps {
   logo: string;
+  isTable?: boolean;
 }
 
 const LabelLogo: FC<iProps> = (prop) => {
   const logos = {
-    hons: { src: HonsLogo, alt: "Hons", width: 260 / 3.5, height: 130 / 3.5 },
+    hons: { src: HonsLogo, alt: "Hons", width: prop.isTable ? 260 / 5 : 260 / 3.5, height: prop.isTable ? 130 / 5 : 130 / 3.5 },
     sunningfoods: {
       src: sunningfoods,
       alt: "Sunning foods",
-      width: 335/4,
-      height: 295/4,
+      width: prop.isTable ? 335 / 5 : 335 / 3.5,
+      height: prop.isTable ? 295 / 5 : 295 / 3.5,
     },
-    shinsenna: { src: shinsenna, alt: "Shinsenna", width: 238/3, height: 296/3 },
-    viethuong: { src: viethuong, alt: "Viet Huong", width: 415/4, height: 158/4 },
+    shinsenna: { src: shinsenna, alt: "Shinsenna", width: prop.isTable ? 238 / 5 : 238 / 3, height: prop.isTable ? 296 / 5 : 296 / 3},
+    viethuong: { src: viethuong, alt: "Viet Huong", width: prop.isTable ? 415 / 5 : 415 / 4, height: prop.isTable ? 158 / 5 : 158 / 4 },
   };
 
   const logoKey = (prop.logo in logos ? prop.logo : "viethuong") as keyof typeof logos;
